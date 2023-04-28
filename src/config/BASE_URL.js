8ccc9979c6fc9ba8aa5099671bec05c73c063402
Awesh Choudhary <aweshchoudhary7@gmail.com>
2023-04-28 15:08:33 +0530

    base url changed

--- src/config/BASE_URL.js
+++ src/config/BASE_URL.js
@@ -1,2 +1,2 @@
-const baseurl = "http://localhost:5000/api"; //"https://u92x7hCo3FVb.stellaraesthetics.in/api";
+const baseurl = "https://u92x7hCo3FVb.stellaraesthetics.in/api"; //"http://localhost:5000/api"; //;
 export default baseurl;
